c712573a6cfdbe45dd20e176e451865c3d4c4454
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-20 19:07:03 +0300

    update search_engine.py for search compound queries

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -3,13 +3,22 @@ import re
 
 def search(docs, query):
     result = []
-    query_term = re.findall(r'\w+', query)[0]
+    query_terms = re.findall(r'\w+', query)
 
     for doc in docs:
         all_terms = re.findall(r'\w+', doc['text'])
-        if query_term in all_terms:
-            relevance = all_terms.count(query_term)
+        relevance = {'match': 0, 'count': 0}
+
+        for query_term in query_terms:
+            if query_term in all_terms:
+                relevance['match'] += 1
+                relevance['count'] += all_terms.count(query_term)
+
+        if relevance['match']:
             result.append({'id': doc['id'], 'relevance': relevance})
 
-    sorted_result = sorted(result, key=lambda i: i['relevance'], reverse=True)
+    sorted_result = sorted(
+        result,
+        key=lambda i: (i['relevance']['match'], i['relevance']['count']),
+        reverse=True)
     return [item['id'] for item in sorted_result]
